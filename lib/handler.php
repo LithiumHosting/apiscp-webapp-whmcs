200012733237c6893b2c55136fa8537d894ba7db
Matt Saladna <msaladna@apisnetworks.com>
2022-04-07 18:47:55 -0400

    Reformat

--- lib/handler.php
+++ lib/handler.php
@@ -1,106 +1,103 @@
-<?php
-declare(strict_types=1);
-/**
- * Copyright (C) Lithium Hosting, llc - All Rights Reserved.
- *
- * Unauthorized copying of this file, via any medium, is
- * strictly prohibited without consent. Any dissemination of
- * material herein is prohibited.
- *
- * For licensing inquiries email <tsiedsma@lithiumhosting.com>
- *
- * Written by Troy Siedsma <tsiedsma@lithiumhosting.com>, December 2020
- */
+<?php declare(strict_types=1);
+
+	/**
+	 * Copyright (C) Lithium Hosting, llc - All Rights Reserved.
+	 *
+	 * Unauthorized copying of this file, via any medium, is
+	 * strictly prohibited without consent. Any dissemination of
+	 * material herein is prohibited.
+	 *
+	 * For licensing inquiries email <tsiedsma@lithiumhosting.com>
+	 *
+	 * Written by Troy Siedsma <tsiedsma@lithiumhosting.com>, December 2020
+	 */
 
 
-namespace lithiumhosting\whmcs;
+	namespace lithiumhosting\whmcs;
 
 
-use Module\Support\Webapps\App\Type\Unknown\Handler as Unknown;
+	use Module\Support\Webapps\App\Type\Unknown\Handler as Unknown;
 
-class Handler extends Unknown {
+	class Handler extends Unknown
+	{
 
-    const NAME                  = 'WHMCS';
-    const ADMIN_PATH            = '/admin';
-    const LINK                  = 'https://whmcs.com';
-    const DEFAULT_FORTIFICATION = 'max';
+		const NAME = 'WHMCS';
+		const ADMIN_PATH = '/admin';
+		const LINK = 'https://whmcs.com';
+		const DEFAULT_FORTIFICATION = 'max';
 
-    const FEAT_ALLOW_SSL = true;
-    const FEAT_RECOVERY  = false;
+		const FEAT_ALLOW_SSL = true;
+		const FEAT_RECOVERY = false;
 
-    /**
-     * Display application
-     *
-     * @return bool
-     */
-    public function display(): bool
-    {
-        return true;
-    }
+		/**
+		 * Display application
+		 *
+		 * @return bool
+		 */
+		public function display(): bool
+		{
+			return true;
+		}
 
-    public function hasInstall(): bool
-    {
-        return false;
-    }
+		public function hasInstall(): bool
+		{
+			return false;
+		}
 
-    /**
-     * Get available versions
-     *
-     * @return array|string[]
-     */
-    public function getVersions(): array
-    {
-        return ['1.0'];
-    }
+		/**
+		 * Get available versions
+		 *
+		 * @return array|string[]
+		 */
+		public function getVersions(): array
+		{
+			return ['1.0'];
+		}
 
-    /**
-     * Wrapper to show API calls
-     *
-     * @param string $method
-     * @param null   $args
-     *
-     * @return bool
-     */
-    public function __call($method, $args = null)
-    {
-        if (false === strpos($method, '_'))
-        {
-            return parent::__call($method, $args);
-        }
-        [$module, $fn] = explode('_', $method, 2);
-        if ($module !== $this->getClassMapping())
-        {
-            return parent::__call($method, $args);
-        }
+		/**
+		 * Wrapper to show API calls
+		 *
+		 * @param string $method
+		 * @param null   $args
+		 *
+		 * @return bool
+		 */
+		public function __call($method, $args = null)
+		{
+			if (false === strpos($method, '_')) {
+				return parent::__call($method, $args);
+			}
+			[$module, $fn] = explode('_', $method, 2);
+			if ($module !== $this->getClassMapping()) {
+				return parent::__call($method, $args);
+			}
 
-        if (is_debug())
-        {
-            // optionally report all module calls on-screen as encountered
-            // enable debug mode first,
-            // cpcmd scope:set cp.debug true
-            echo $module, ': ', $fn, "\n";
-        }
+			if (is_debug()) {
+				// optionally report all module calls on-screen as encountered
+				// enable debug mode first,
+				// cpcmd scope:set cp.debug true
+				echo $module, ': ', $fn, "\n";
+			}
 
-        return parent::__call($method, $args);
-    }
+			return parent::__call($method, $args);
+		}
 
-    /**
-     * API module that calls flow through
-     *
-     * @return string
-     */
-    public function getClassMapping(): string
-    {
-        return 'liwhmcs';
-    }
+		/**
+		 * API module that calls flow through
+		 *
+		 * @return string
+		 */
+		public function getClassMapping(): string
+		{
+			return 'liwhmcs';
+		}
 
-    public function handle(array $params): bool
-    {
-        if (! empty($params['say']))
-        {
-            return success("Saying something nice: %s", $this->{$this->getClassMapping() . '_hello'}());
-        }
+		public function handle(array $params): bool
+		{
+			if (!empty($params['say'])) {
+				return success("Saying something nice: %s", $this->{$this->getClassMapping() . '_hello'}());
+			}
 
-        return parent::handle($params);
-    }
-}
+			return parent::handle($params);
+		}
+	}
--- lib/module.php
+++ lib/module.php
@@ -1,184 +1,177 @@
 <?php declare(strict_types=1);
 
-namespace lithiumhosting\whmcs;
+	namespace lithiumhosting\whmcs;
 
-use Module\Support\Webapps;
+	use Module\Support\Webapps;
 
-class Whmcs_Module extends Webapps {
-
-    const APP_NAME = 'WHMCS';
-	const VERSION_CHECK_URL = 'https://api1.whmcs.com/download/latest';
-
-    protected $aclList = [
-        'min' => [
-            'attachments',
-            'downloads',
-            'templates_c',
-            '.htaccess',
-        ],
-        'max' => [
-            'attachments',
-            'downloads',
-            'templates_c',
-        ],
-    ];
-
-    /**
-     * Installed app is type "whmcs"
-     *
-     * @param string $mixed
-     * @param string $path
-     *
-     * @return bool
-     */
-    public function valid(string $mixed, string $path = ''): bool
-    {
-
-        // $mixed is passed as [hostname, path] combination
-        // convert to filesystem path
-        if ($mixed[0] !== '/')
-        {
-            $mixed = $this->getDocumentRoot($mixed, $path);
-        }
-
-        return file_exists($this->domain_fs_path($mixed) . '/vendor/whmcs/whmcs-foundation/lib/License.php');
-    }
-
-    /**
-     * Install application
-     *
-     * @param string $hostname
-     * @param string $path
-     * @param array  $opts
-     *
-     * @return bool
-     */
-    public function install(string $hostname, string $path = '', array $opts = []): bool
-    {
-        if (! $this->mysql_enabled())
-        {
-            return error('%(what)s must be enabled to install %(app)s',
-                ['what' => 'MySQL', 'app' => static::APP_NAME]);
-        }
-
-        if (! $this->parseInstallOptions($opts, $hostname, $path))
-        {
-            return false;
-        }
-
-        $docroot  = $this->getDocumentRoot($hostname, $path);
-        $contents = '1.0';
-
-        if (! empty($opts['say']))
-        {
-            // in module API call, enabled by specifying ['hello' => true] in $opts
-            $contents .= ' ' . $this->hello();
-        }
-
-        $oldex   = \Error_Reporter::exception_upgrade(\Error_Reporter::E_ERROR);
-        $success = false;
-        try
-        {
-            $success = $this->file_put_file_contents($docroot . '/foo', $contents);
-            // set Web App meta data, used for inference as to what app is installed here
-            $this->initializeMeta($docroot, $opts);
-        }
-        finally
-        {
-            // Restore ER exception promotion
-            \Error_Reporter::exception_upgrade($oldex);
-            if (! $success)
-            {
-                return false;
-            }
-        }
-
-        // Apply Fortification, useful with PHP applications which run under a different UID
-        // see Fortification.md
-        $this->fortify($hostname, $path, 'max');
-
-        // Send notification email
-        $this->notifyInstalled($hostname, $path, $opts);
-
-        return true;
-    }
-
-    /**
-     * An extra API method available via "demo2:hello"
-     *
-     * @return string
-     */
-    public function hello(): string
-    {
-        return "beautiful!";
-    }
-
-    /**
-     * Remove application
-     *
-     * @param string $hostname
-     * @param string $path
-     * @param string $delete
-     *
-     * @return bool
-     */
-    public function uninstall(string $hostname, string $path = '', $delete = 'all'): bool
-    {
-        // parent does a good job of removing all traces, you can do any last minute touch-ups, such as
-        // removing Redis services or changing DNS after uninstallation
-        return parent::uninstall($hostname, $path, $delete);
-    }
-
-    /**
-     * Get available versions
-     *
-     * Used to determine whether an app is eligible for updates
-     *
-     * @return array|string[]
-     */
-    public function get_versions(): array
-    {
-		return (array)array_get($this->_getVersions(), 'version', []);
-    }
-
-	private function _getVersions(): array
+	class Whmcs_Module extends Webapps
 	{
-		$cache = \Cache_Super_Global::spawn();
-		if (false !== ($versions = $cache->get('whmcs.versions'))) {
+
+		const APP_NAME = 'WHMCS';
+		const VERSION_CHECK_URL = 'https://api1.whmcs.com/download/latest';
+
+		protected $aclList = [
+			'min' => [
+				'attachments',
+				'downloads',
+				'templates_c',
+				'.htaccess',
+			],
+			'max' => [
+				'attachments',
+				'downloads',
+				'templates_c',
+			],
+		];
+
+		/**
+		 * Installed app is type "whmcs"
+		 *
+		 * @param string $mixed
+		 * @param string $path
+		 *
+		 * @return bool
+		 */
+		public function valid(string $mixed, string $path = ''): bool
+		{
+
+			// $mixed is passed as [hostname, path] combination
+			// convert to filesystem path
+			if ($mixed[0] !== '/') {
+				$mixed = $this->getDocumentRoot($mixed, $path);
+			}
+
+			return file_exists($this->domain_fs_path($mixed) . '/vendor/whmcs/whmcs-foundation/lib/License.php');
+		}
+
+		/**
+		 * Install application
+		 *
+		 * @param string $hostname
+		 * @param string $path
+		 * @param array  $opts
+		 *
+		 * @return bool
+		 */
+		public function install(string $hostname, string $path = '', array $opts = []): bool
+		{
+			if (!$this->mysql_enabled()) {
+				return error('%(what)s must be enabled to install %(app)s',
+					['what' => 'MySQL', 'app' => static::APP_NAME]);
+			}
+
+			if (!$this->parseInstallOptions($opts, $hostname, $path)) {
+				return false;
+			}
+
+			$docroot = $this->getDocumentRoot($hostname, $path);
+			$contents = '1.0';
+
+			if (!empty($opts['say'])) {
+				// in module API call, enabled by specifying ['hello' => true] in $opts
+				$contents .= ' ' . $this->hello();
+			}
+
+			$oldex = \Error_Reporter::exception_upgrade(\Error_Reporter::E_ERROR);
+			$success = false;
+			try {
+				$success = $this->file_put_file_contents($docroot . '/foo', $contents);
+				// set Web App meta data, used for inference as to what app is installed here
+				$this->initializeMeta($docroot, $opts);
+			} finally {
+				// Restore ER exception promotion
+				\Error_Reporter::exception_upgrade($oldex);
+				if (!$success) {
+					return false;
+				}
+			}
+
+			// Apply Fortification, useful with PHP applications which run under a different UID
+			// see Fortification.md
+			$this->fortify($hostname, $path, 'max');
+
+			// Send notification email
+			$this->notifyInstalled($hostname, $path, $opts);
+
+			return true;
+		}
+
+		/**
+		 * An extra API method available via "demo2:hello"
+		 *
+		 * @return string
+		 */
+		public function hello(): string
+		{
+			return "beautiful!";
+		}
+
+		/**
+		 * Remove application
+		 *
+		 * @param string $hostname
+		 * @param string $path
+		 * @param string $delete
+		 *
+		 * @return bool
+		 */
+		public function uninstall(string $hostname, string $path = '', $delete = 'all'): bool
+		{
+			// parent does a good job of removing all traces, you can do any last minute touch-ups, such as
+			// removing Redis services or changing DNS after uninstallation
+			return parent::uninstall($hostname, $path, $delete);
+		}
+
+		/**
+		 * Get available versions
+		 *
+		 * Used to determine whether an app is eligible for updates
+		 *
+		 * @return array|string[]
+		 */
+		public function get_versions(): array
+		{
+			return (array)array_get($this->_getVersions(), 'version', []);
+		}
+
+		private function _getVersions(): array
+		{
+			$cache = \Cache_Super_Global::spawn();
+			if (false !== ($versions = $cache->get('whmcs.versions'))) {
+				return $versions;
+			}
+
+			$contents = file_get_contents(self::VERSION_CHECK_URL);
+			if (!($versions = json_decode($contents, true))) {
+				return array();
+			}
+
+			$cache->set('whmcs.versions', $versions);
+
 			return $versions;
 		}
 
-		$contents = file_get_contents(self::VERSION_CHECK_URL);
-		if (!($versions = json_decode($contents, true))) {
-			return array();
+		public function get_version(string $hostname, string $path = ''): ?string
+		{
+			$path = $this->getDocumentRoot($hostname, $path) . '/foo';
+
+			// install file missing?
+			if (!$this->file_exists($this->domain_fs_path($path))) {
+				return null;
+			}
+
+			return strtok($this->domain_fs_path($path), ' ');
 		}
 
-		$cache->set('whmcs.versions', $versions);
-		return $versions;
+		/**
+		 * Get Web App handler name
+		 *
+		 * @return string
+		 * @throws \ReflectionException
+		 */
+		public function getModule(): string
+		{
+			// fallback Web App handler name when "type" isn't specified in install
+			return parent::getModule();
+		}
 	}
-
-    public function get_version(string $hostname, string $path = ''): ?string
-    {
-        $path = $this->getDocumentRoot($hostname, $path) . '/foo';
-
-        // install file missing?
-        if (! $this->file_exists($this->domain_fs_path($path)))
-        {
-            return null;
-        }
-
-        return strtok($this->domain_fs_path($path), ' ');
-    }
-
-    /**
-     * Get Web App handler name
-     *
-     * @return string
-     * @throws \ReflectionException
-     */
-    public function getModule(): string
-    {
-        // fallback Web App handler name when "type" isn't specified in install
-        return parent::getModule();
-    }
-}
