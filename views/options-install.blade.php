f29f2b5bab975c417d5035ae870ca8e41513aa64
Troy Siedsma <tsiedsma@lithiumhosting.com>
2022-04-13 13:12:29 -0500

    Update install fields, make them required

--- views/options-install.blade.php
+++ views/options-install.blade.php
@@ -1,9 +1,9 @@
 <div class="form-group">
 	<label>License Key</label>
-	<input type="text" class="form-control" name="license_key" />
+	<input type="text" class="form-control" name="license_key" required />
 </div>
 
 <div class="form-group">
 	<label>Admin Username</label>
-	<input type="text" class="form-control" name="whmcs_username" value="admin" />
+	<input type="text" class="form-control" name="whmcs_username" value="admin" required />
 </div>
